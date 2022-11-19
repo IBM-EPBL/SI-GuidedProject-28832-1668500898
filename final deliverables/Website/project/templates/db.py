a2986b42d3d87a9ee6d2c95e26d562336890a7a2
Akashchowdarylabala <117619702+Akashchowdarylabala@users.noreply.github.com>
2022-11-19 14:26:29 +0530

    Delete final deliverables/Website/project directory

--- final deliverables/Website/project/templates/db.py
+++
@@ -1,5 +0,0 @@
-import ibm_db
-conn = ibm_db.connect("DATABASE=bludb;HOSTNAME=;PORT=;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID=;PWD=",'','')
-
-print(conn)
-print("connection successful...")
